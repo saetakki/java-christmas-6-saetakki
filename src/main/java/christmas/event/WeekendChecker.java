f190fae17d1cea0a9b7e16611609f84a5975ad4f
세탁기 <116627038+saetakki@users.noreply.github.com>
2023-11-12 23:36:11 +0900

    feat: 특정 요일의 주말 여부 리턴하는 함수 생성 1. 특정 날짜를 입력받아서 해당 날이 주말인지 여부를 리턴하는 함수 생성 2. 주말에 해당할 경우 true 아닐경우 false 리턴 "

--- src/main/java/christmas/event/WeekendChecker.java
+++ src/main/java/christmas/event/WeekendChecker.java
@@ -1,11 +1,11 @@
-package event;
+package christmas.event;
 import java.time.LocalDate;
 import java.time.DayOfWeek;
 
 public class WeekendChecker {
-    public static boolean isWeekend(LocalDate date){
-        DayOfWeek day = date.getDayOfWeek();
-        return day == DayOfWeek.FRIDAY || day == DayOfWeek.SATURDAY;
+    public static boolean isWeekend(int date){
+        int dayOfWeek = date % 7;
+        return dayOfWeek == 1 || dayOfWeek == 2;
     }
 
 }
