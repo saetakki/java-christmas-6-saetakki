50b951f3e52971ffc14749aabfa3c9ba21f38fa0
세탁기 <116627038+saetakki@users.noreply.github.com>
2023-11-13 17:32:40 +0900

    feat: InputView 생성 1. InputVisitDay를 통해서 방문일 입력 기능 추가 2. inputMenu를 통해서 메뉴와 수량 입력기능 추가

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,17 @@
+package christmas.view;
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static int inputVisitDay(){
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        String dateInput = Console.readLine();
+
+        return Integer.parseInt(dateInput);
+    }
+
+    public static String inputMenu(){
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        return Console.readLine();
+    }
+}
