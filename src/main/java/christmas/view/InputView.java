0b6b0d3efa0cffccdf34e95a3763fe69a05ba0d7
세탁기 <116627038+saetakki@users.noreply.github.com>
2023-11-14 00:09:16 +0900

    feat: 예외 발생 시 재입력 1. 방문 날자 입력 시 1~31 초과하면 에러 메세지 출력하고 재입력 2. 방문 날짜 입력 시 문자열 입력하면 에러 메세지 출력하고 재입력 3. 주문 메뉴 입력할 때 입력형식 틀리면 재입력 4. 주문 메뉴 입력할 때 중복 메뉴 있으면 재입력 5. 주문 메뉴 입력할 때 수량 초과하면 재입력 6. 주문 메뉴 입력할 때 오류 발생시 에러 메세지 출력

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,16 +1,42 @@
 package christmas.view;
-import camp.nextstep.edu.missionutils.Console;
+
+import static christmas.validator.EventValidator.validateQuantityDoesNotExceed;
+import static christmas.validator.EventValidator.validateUniqueItems;
+import static christmas.validator.InputValidator.validateMenuInputForm;
+import static christmas.validator.InputValidator.validateSeperatedByComma;
+import static christmas.validator.InputValidator.validateWithinRange;
+import static christmas.view.InputPrompt.promptAndGetInput;
+
+import christmas.Constants.ErrorMessage;
 
 public class InputView {
 
-    public static int inputVisitDay(){
-        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        String dateInput = Console.readLine();
+    public static int inputVisitDay() {
+        while (true) {
+            String dateInput = promptAndGetInput("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+            try {
+                validateWithinRange(dateInput,1,31);
+                return Integer.parseInt(dateInput);
+            } catch (IllegalArgumentException e) {
+                System.out.println(ErrorMessage.INVALID_DATE.getMessage());
+            }
+        }
+    }
 
-        return Integer.parseInt(dateInput);
-    }
-    public static String inputMenu(){
-        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
-        return Console.readLine();
-    }
+
+        public static String inputMenu() {
+            while (true) {
+                String menuInput = promptAndGetInput("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+                try {
+                    validateMenuInputForm(menuInput);
+                    validateSeperatedByComma(menuInput);
+                    validateUniqueItems(menuInput);
+                    validateQuantityDoesNotExceed(menuInput, 20);
+                    return menuInput;
+                } catch (IllegalArgumentException e) {
+                    System.out.println(ErrorMessage.INVALID_ORDER.getMessage());
+                }
+            }
+        }
+
 }
