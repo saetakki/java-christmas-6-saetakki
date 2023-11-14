0591b55c46d4503b7c67a4e29d648a4fd9056c89
세탁기 <116627038+saetakki@users.noreply.github.com>
2023-11-14 13:29:04 +0900

    refactor: 불필요한 라인 삭제

--- src/main/java/christmas/event/dateChecker.java
+++ src/main/java/christmas/event/dateChecker.java
@@ -1,13 +1,24 @@
 package christmas.event;
-import java.time.LocalDate;
-import java.time.DayOfWeek;
 
-public class WeekendChecker {
+public class dateChecker {
     public static boolean isWeekend(int date){
-        if( date > 31 || date < 1)
-            throw new IllegalArgumentException("맞지않는 범위");
         int dayOfWeek = date % 7;
         return dayOfWeek == 1 || dayOfWeek == 2;
+    }
+
+
+    public static boolean isSundayOrChristmas(int date){
+        int HappySunday = date % 7;
+        int MerryChristmas = 25;
+        return HappySunday == 3 || date == MerryChristmas;
+    }
+
+    public static String todayIs(int date) {
+        String today = "평일";
+        if (isWeekend(date)) {
+            today = "주말";
+        }
+        return today;
     }
 
 }
