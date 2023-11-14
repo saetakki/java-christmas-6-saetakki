3f4fece39515738e280b33dd2adc048cfa2944dc
세탁기 <116627038+saetakki@users.noreply.github.com>
2023-11-14 13:22:36 +0900

    refactor: 불필요한 라인 삭제

--- src/main/java/christmas/event/DiscountCalculator.java
+++ src/main/java/christmas/event/DiscountCalculator.java
@@ -1,19 +1,31 @@
 package christmas.event;
 
+import static christmas.Constants.DISCOUNT_AMOUNT;
+import static christmas.Constants.MINIMUM_SPEND_FOR_CHAMPAGNE;
 import static christmas.data.Foods.FoodItem.getPriceOfMenu;
-import static christmas.event.VisitDayDiscountUntilChristmas.applyAdditionalDiscount;
+import static christmas.event.CostCalculator.calculateOriginalPricePerItem;
+import static christmas.event.SpecialDayDiscountCalculator.applySpecialDayAndDdayDiscount;
+import static christmas.event.DiscountConditionChecker.isEligibleForDiscount;
 
 import java.util.Map;
 import java.util.function.Function;
 
 public class DiscountCalculator {
-
     // 날짜에 따른 할인을 적용한 후의 총 예상 결제금액을 계산하는 함수
     // 날짜별 할인 금액을 계산한 후, 1일부터 25일까지 진행되는 방문일 할인, 주말 및 크리스마스 당일의 추가할인 순으로 계산
-    private static int calculateTotalDiscountedPrice(Map<String, Integer> cart, Integer date) {
+    public static int calculateTotalDiscountedPrice(Map<String, Integer> cart, Integer date) {
         int basicDiscountAppliedPrice = cart.entrySet().stream().map(calculatePriceWithDiscountStrategy(date)).reduce(0, (a, b) -> a + b);
-        Function<Integer, Integer> AdditionalDiscountApplier = applyAdditionalDiscount(date);
+        Function<Integer, Integer> AdditionalDiscountApplier = applySpecialDayAndDdayDiscount(date);
         return AdditionalDiscountApplier.apply(basicDiscountAppliedPrice);
+    }
+
+
+    public static int calculateGiftPrice(Integer originalPrice, String gift){
+        int GIFT_PRICE = getPriceOfMenu(gift);
+        if(originalPrice >= MINIMUM_SPEND_FOR_CHAMPAGNE){
+            return originalPrice-GIFT_PRICE;
+        }
+        return originalPrice;
     }
 
     // 날짜에 따라 장바구니 각 항목에 적용할 가격 계산 전략을 결정하는 함수
@@ -30,7 +42,8 @@ public class DiscountCalculator {
     // 장바구니의 각 항목 할인가를 계산
     // getPriceOfMenu를 통해 해당 항목의 원가를 호출하고 수량(entry.getValue())를 곱해 항목 총액을 계산하되
     // 원가에 할인가를 적용하여 할인이 적용된 가격을 계산
-    private static Function<Map.Entry<String, Integer>, Integer> calculateDiscountedPricePerItem =
-            entry -> (getPriceOfMenu(entry.getKey())-DISCOUNT_AMOUNT) * entry.getValue();
+
+    private static Function<Map.Entry<String, Integer>, Integer> calculateDiscountedPricePerItem = entry ->
+            (getPriceOfMenu(entry.getKey()) - DISCOUNT_AMOUNT) * entry.getValue();
 
 }
--- src/main/java/christmas/event/DiscountConditionChecker.java
+++ src/main/java/christmas/event/DiscountConditionChecker.java
@@ -16,15 +16,9 @@ public class DiscountConditionChecker {
 
     // 메뉴 이름과 날짜를 인자로 받아 해당 품목의 할인 적용 여부를 판단하는 함수
     // 주말 - 메인코스 | 평일 - 디저트에 할인 적용
-    // 메뉴 항목이 존재하지 않는 경우 예외처리
     public static boolean isEligibleForDiscount(String menu, Integer date) {
-        try {
-            FoodItem foodItem = FoodItem.valueOf(menu);
-            return (isMainCourse.test(foodItem) && isWeekend(date)) || (isDessert.test(foodItem) && !isWeekend(date));
-
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("에러발생");
-        }
+        FoodItem foodItem = FoodItem.valueOf(menu);
+        return (isMainCourse.test(foodItem) && isWeekend(date)) || (isDessert.test(foodItem) && !isWeekend(date));
     }
 }
 
