95b37debcf6698b8e74429dedd9a0066744b9759
세탁기 <116627038+saetakki@users.noreply.github.com>
2023-11-14 13:25:27 +0900

    feat: 혜택 금액 필드 생성 1. 증정품 금액까지 계산하는 혜택 금액을 담을 필드 생성 2. 혜택 금액을 불러오는 메서드 추가

--- src/main/java/christmas/dto/PriceResult.java
+++ src/main/java/christmas/dto/PriceResult.java
@@ -3,10 +3,12 @@ package christmas.dto;
 public class PriceResult {
     private final Integer originalPrice;
     private final Integer discountedPrice;
+    private final Integer includeGiftPrice;
 
-    public PriceResult(Integer originalPrice, Integer discountedPrice){
+    public PriceResult(Integer originalPrice, Integer discountedPrice, Integer includeGiftPrice){
         this.originalPrice = originalPrice;
         this.discountedPrice = discountedPrice;
+        this.includeGiftPrice = includeGiftPrice;
     }
 
     public Integer getOriginalPrice(){
@@ -15,4 +17,5 @@ public class PriceResult {
     public Integer getDiscountedPrice(){
         return discountedPrice;
     }
+    public Integer getIncludeGiftPrice(){ return includeGiftPrice;}
 }
