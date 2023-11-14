70dfad6769d7bc4d565f36faf83f073d73671c01
세탁기 <116627038+saetakki@users.noreply.github.com>
2023-11-14 13:28:23 +0900

    feat: 주문 내역 생성 기능 1. 메뉴의 유효성 검사를 한 후 orderList 반환하는 함수

--- src/main/java/christmas/utils/OrderListManager.java
+++ src/main/java/christmas/utils/OrderListManager.java
@@ -1,12 +1,18 @@
 package christmas.utils;
 
+import static christmas.validator.InputValidator.validateMenuAvailable;
 import christmas.dto.OrderList;
 import java.util.List;
 
 public class OrderListManager {
     public static OrderList createOrderList(List<String[]> parsedInput) {
         OrderList orderList = new OrderList();
-        parsedInput.forEach(data -> orderList.addItem(data[0], Integer.parseInt(data[1])));
+        parsedInput.forEach(data -> {
+            // 메뉴 유효성 검사 후, 유효한 메뉴만 목록에 추가
+            if (validateMenuAvailable(data[0])) {
+                orderList.addItem(data[0], Integer.parseInt(data[1]));
+            }
+        });
         return orderList;
     }
 }
