4a9f8a22d29c45ca81def29a2252aec3371dfe6c
세탁기 <116627038+saetakki@users.noreply.github.com>
2023-11-15 19:21:26 +0900

    refactor: MainController 리팩터링

--- src/main/java/christmas/controller/MainController.java
+++ src/main/java/christmas/controller/MainController.java
@@ -1,37 +1,32 @@
 package christmas.controller;
 
-import static christmas.view.OutputView.printDiscountDetails;
-import static christmas.view.OutputView.printFinalPriceAndBadge;
-import static christmas.view.OutputView.printGreetingMessage;
-import static christmas.view.OutputView.printMenu;
-import static christmas.view.OutputView.printOrder;
-import static christmas.view.OutputView.printPriceDetails;
-
 import christmas.dto.OrderList;
 import christmas.dto.PriceResult;
+import christmas.service.OrderService;
+import christmas.service.PriceService;
 import christmas.view.InputView;
+import christmas.view.OutputView;
 
 public class MainController {
 
-    private static void printOrderDetails(PriceResult priceResult, OrderList orderList, Integer date) {
-        System.out.println("<주문 메뉴>");
-        printOrder(orderList);
-        printPriceDetails(priceResult);
-        printDiscountDetails(priceResult, orderList, date);
-        printFinalPriceAndBadge(priceResult);
-    }
-    private static void printInitalPrint(){
-        printGreetingMessage();
-        printMenu();
+    private final OrderService orderService;
+    private final PriceService priceService;
+
+    public MainController(OrderService orderService, PriceService priceService) {
+        this.orderService = orderService;
+        this.priceService = priceService;
     }
 
-    public static void start() {
-        printInitalPrint();
-        int date = InputView.inputVisitDay();
-        String order = InputView.inputMenu();
-        System.out.println('\n');
-        OrderList orderList = OrderController.createOrderListFromInput(order);
-        PriceResult priceResult = PriceController.calculatePrice(orderList, date);
-        printOrderDetails(priceResult, orderList, date);
+    public void start() {
+        // 초기 화면 출력
+        OutputView.printInitialMessage();
+        // 방문일, 주문 메뉴 입력받기
+        int visitDay = InputView.inputVisitDay();
+        String orderInput = InputView.inputOrderMenu();
+        // 주문리스트, 주문가격 생성
+        OrderList orderList = orderService.createOrderListFromInput(orderInput);
+        PriceResult priceResult = priceService.calculatePrice(orderList, visitDay);
+        // 주문 상세 내역 출력
+        OutputView.printOrderDetails(priceResult, orderList, visitDay);
     }
 }
