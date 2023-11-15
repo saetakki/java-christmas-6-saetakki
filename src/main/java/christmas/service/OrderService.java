bf8ad1793e11f0975fb430a7a837c9dbd969256a
세탁기 <116627038+saetakki@users.noreply.github.com>
2023-11-15 18:14:19 +0900

    refactor: 불필요한 주석 제거

--- src/main/java/christmas/service/OrderService.java
+++ src/main/java/christmas/service/OrderService.java
@@ -1,19 +1,3 @@
-//package christmas.service;
-//
-//import static christmas.utils.InputParser.parseInputToArray;
-//
-//import christmas.dto.OrderList;
-//import christmas.utils.OrderListManager;
-//import java.util.List;
-//
-//public class OrderController {
-//    public static OrderList createOrderListFromInput(String orderInput){
-//        List<String[]> parsedInput = parseInputToArray(orderInput);
-//        return OrderListManager.createOrderList(parsedInput);
-//    }
-//
-//}
-//
 package christmas.service;
 
 import christmas.dto.OrderList;
